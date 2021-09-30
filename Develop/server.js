2d44832b44663d814d77f9449d9090b8055e1ed9
JD Rowley <jdrowley1989@gmail.com>
2021-09-30 14:28:04 -0600

    add dynamic port

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const notes = require('./db/db');
@@ -9,6 +11,6 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
